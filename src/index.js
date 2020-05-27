d9746e09c0f1970bb035eecd5675376333d746aa
Janet Zheng <janet.zhengg@gmail.com>
2020-05-27 09:49:26 -0700

    imports successful. called components in App.jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import ReactDOM from 'react-dom'; // This is needed to use ReactDOM.render();
-import React from 'react'; // This is needed if you want to use JSX syntax or extends React.Component
-
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
-ReactDOM.render(<App />, document.GetElementByID('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
