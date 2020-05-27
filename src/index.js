59a31cbb0f9abc54bbfd16626bb9cc631931ea98
Won Gyun Park <park.wongyun@gmail.com>
2020-05-26 22:12:08 -0700

    Test Push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import ReactDOM from 'react-dom'; // This is needed to use ReactDOM.render();
+import React from 'react'; // This is needed if you want to use JSX syntax or extends React.Component
+
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+ReactDOM.render(<App />, document.GetElementByID('app'));
